8825b74e66be7c2b67202ebc3a0a37d003b8435d
thesuhas <suhastvs@gmail.com>
2022-07-14 21:57:53 +0530

    please help

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -1,16 +1,29 @@
 from urllib import response
 from flask import Flask, request, jsonify
-import py_compile
 import sys
+import os
+import subprocess
 
 app = Flask(__name__)
 
 @app.route('/sanity-check', methods=["POST"])
 def sanity_check():
-    files = request.files
-    print(files.getlist('File'), file=sys.stderr)
+    files = request.files.to_dict()
+    file = files['File']
+    
+    file.save(file.filename)
+    process = subprocess.run(['pylint', '--disable=I,R,C,W', f'{file.filename}'], stdout=subprocess.PIPE)
+    os.system(f'rm {file.filename}')
+    
 
-    return jsonify("received")
+    if "error" in process.stdout.decode('utf-8'):
+        message = "Error"
+        status_code = 400
+    else:
+        message = "No Error"
+        status_code = 200
+
+    return jsonify({'message': message}), status_code
 
 
 if __name__ == "__main__":
