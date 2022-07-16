aedf7f89dbfe50598ce300e24b23d66f6fa2395a
thesuhas <suhastvs@gmail.com>
2022-07-16 11:57:15 +0530

    fixed a few issues;

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -1,56 +1,47 @@
+from functools import reduce
 from urllib import response
 from flask import Flask, request, jsonify
+from flask_cors import cross_origin
 import requests
 import sys
 import os
 import subprocess
 from signal import signal, SIGPIPE, SIG_DFL
 signal(SIGPIPE, SIG_DFL)
+import json
 
 app = Flask(__name__)
 
+
 @app.route('/sanity-check', methods=["POST"])
+@cross_origin()
 def sanity_check():
     '''
     Currently assuming the assignment to be a MR Job
     '''
-    files = request.files.to_dict()
-    mapper = files['Mapper']
-    reducer = files['Reducer']
-    team_ID = request.form.get('Team ID')
-    assignment_ID = request.form.get('Assignment ID')
-    timeout = request.form.get('Timeout')
-    task = request.form.get('Task')
+    data = json.loads(request.data)
 
-    #print(request.get_json(), file=sys.stderr)
-    #print(request.form, file=sys.stderr)
-    # Mapper part
-    mapper.save(mapper.filename)
-    process = subprocess.Popen(['pylint', '--disable=I,R,C,W', f'{mapper.filename}'], stdout=subprocess.PIPE)
+    mapper_data = data["mapper"]
+    reducer_data = data['reducer']
+
+    mapper = open('mapper.py', 'w')
+    mapper.write(mapper_data)
+    mapper.close()
+    reducer = open('reducer.py', 'w')
+    reducer.write(reducer_data)
+    reducer.close()
+
+    process = subprocess.Popen(['pylint', '--disable=I,R,C,W', 'mapper.py'], stdout=subprocess.PIPE)
     mapper_output = process.communicate()[0]
-    #print(mapper, file=sys.stderr)
-    #print(type(output))
-    os.system(f'rm {mapper.filename}')
-    # Reducer Part
-    reducer.save(reducer.filename)
-    process = subprocess.Popen(['pylint', '--disable=I,R,C,W', f'{reducer.filename}'], stdout=subprocess.PIPE)
+    os.system('rm mapper.py')
+    process = subprocess.Popen(['pylint', '--disable=I,R,C,W', 'reducer.py'], stdout=subprocess.PIPE)
     reducer_output = process.communicate()[0]
-    #print(type(output))
-    os.system(f'rm {reducer.filename}')
-    
-    #print(output, file=sys.stderr)
+    os.system('rm reducer.py')
+
     if "error" in mapper_output.decode('utf-8') or "error" in reducer_output.decode('utf-8'):
-        message = "Error"
-        status_code = 400
-    else:
-        message = "No Error"
-        status_code = 200
-        # Send request to submit job
-        #print(request.form.to_dict(flat=True), file=sys.stderr)
-        requests.post('http://localhost:10001/submit-job', data=request.form.to_dict(flat=True), files={"Mapper": mapper, "Reducer": reducer})
+        return jsonify({"msg": "error"})
 
-    return jsonify({'message': message}), status_code
-
+    return "received"
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
